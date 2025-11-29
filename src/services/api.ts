322318058cd9e8d63f876bb050e5b822ff6b80b8
Christian O. Fega <52633007+krixfega@users.noreply.github.com>
2025-11-29 16:30:09 +0100

    port fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api/v1', // Primary: Localhost for testing
-  // baseURL: 'https://prop-mgt-backend-api-production.up.railway.app/api/v1', // Secondary: Production
+  // baseURL: 'http://localhost:4005/api/v1', // Primary: Localhost for testing
+  baseURL: 'https://prop-mgt-backend-api-production.up.railway.app/api/v1', // Secondary: Production
 });
 
 api.interceptors.request.use((config) => {
